07f90f27dfd4e189b66d980e6ab0033716df6930
erluzi <747877078@qq.com>
2018-06-22 16:38:12 +0800

    readDir.js

---
+++ __tests__/test_readDir.js
@@ -0,0 +1,19 @@
+let fs = require('fs');
+let path = require('path');
+let fileFor = require('../src/readDir');
+
+let filePath = path.resolve(__dirname, '../src');
+let outPut = 'out.txt';
+
+function readFile(filedir, filename) {
+    if (filename === '.DS_Store') return false;
+    let str = fs.readFileSync(filedir, 'utf-8');
+    fs.writeFileSync(outPut, filename + ' =>\n' + str + '\n', {flag: 'a'})
+}
+
+test('fileFor', () => {
+    return fileFor(filePath, /(.vue|.js)$/, readFile)
+        .then(data => {
+            expect(data).toBe('fileFor execute over!')
+        })
+});
